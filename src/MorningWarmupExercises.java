3464bd3ad24d30a4615ebf46f0b935c87c3ff959
Miguel Vera <miguel.vera7908@gmail.com>
2020-01-31 10:54:11 -0600

    Daily Java Exercise

--- src/MorningWarmupExercises.java
+++ src/MorningWarmupExercises.java
@@ -5,8 +5,12 @@ public class MorningWarmupExercises {
 
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println(multiply(5, 4));
 
-        System.out.println("Enter a number: ");
+
+
+
+//        System.out.println("Enter a number: ");
         long counter = scanner.nextLong();
 
         long count = 0;
@@ -14,6 +18,24 @@ public class MorningWarmupExercises {
         for(int i = 1; i <= counter; i++){
              count += i;
         }
-        System.out.print(count);
+//        System.out.print(count);
+
+//        System.out.println(cube(3));
+
+
     }
+
+    public static long cube(int num){
+
+        return num * num * num;
+    }
+
+    public static long multiply(long x, long y){
+        long result = 0;
+        for(long i = 0; i < x; i++){
+            result += y;
+        }
+        return result;
+    }
+
 }
