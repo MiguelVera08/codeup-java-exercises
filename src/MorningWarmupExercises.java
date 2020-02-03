45486ddf4e87e84fc359714c03e54c4b3944da6a
Miguel Vera <miguel.vera7908@gmail.com>
2020-02-03 09:28:27 -0600

    Java morning exercise

--- src/MorningWarmupExercises.java
+++ src/MorningWarmupExercises.java
@@ -5,8 +5,8 @@ public class MorningWarmupExercises {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(multiply(5, 4));
-
+//        System.out.println(multiply(5, 4));
+        System.out.println(addBinary("10", "11"));
 
 
 
@@ -25,17 +25,27 @@ public class MorningWarmupExercises {
 
     }
 
-    public static long cube(int num){
+//    public static long cube(int num){
+//
+//        return num * num * num;
+//    }
 
-        return num * num * num;
-    }
+//    public static long multiply(long x, long y){
+//        long result = 0;
+//        for(long i = 0; i < x; i++){
+//            result += y;
+//        }
+//        return result;
+//    }
 
-    public static long multiply(long x, long y){
-        long result = 0;
-        for(long i = 0; i < x; i++){
-            result += y;
-        }
-        return result;
+    public static String addBinary(String input0, String input1){
+
+        int numberOne = Integer.parseInt(input0, 2);
+        int numberTwo = Integer.parseInt(input1, 2);
+
+        int sum = numberOne + numberTwo;
+
+        return Integer.toBinaryString(sum);
     }
 
 }
