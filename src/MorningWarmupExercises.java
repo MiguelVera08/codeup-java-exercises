ca5f9a81bda79652175449208fe9e737fe4a4643
Miguel Vera <miguel.vera7908@gmail.com>
2020-02-07 10:18:29 -0600

    Java morning exercise

--- src/MorningWarmupExercises.java
+++ src/MorningWarmupExercises.java
@@ -1,4 +1,7 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
+
 
 public class MorningWarmupExercises {
     public static void main(String[] args){
@@ -6,21 +9,29 @@ public class MorningWarmupExercises {
         Scanner scanner = new Scanner(System.in);
 
 //        System.out.println(multiply(5, 4));
-        System.out.println(addBinary("10", "11"));
+//        System.out.println(addBinary("10", "11"));
 
 
 
 //        System.out.println("Enter a number: ");
-        long counter = scanner.nextLong();
-
-        long count = 0;
-
-        for(int i = 1; i <= counter; i++){
-             count += i;
-        }
+//        long counter = scanner.nextLong();
+//
+//        long count = 0;
+//
+//        for(int i = 1; i <= counter; i++){
+//             count += i;
+//        }
 //        System.out.print(count);
 
 //        System.out.println(cube(3));
+        ArrayList<String> f = new ArrayList<>();
+        String[] fruits = {"Kiwi","Apple","Grapes","Banana","Guava","Pear"};
+
+
+        for (String fruit : fruits) {
+            f.add(fruit);
+        }
+        System.out.println(f);
 
 
     }
@@ -38,14 +49,13 @@ public class MorningWarmupExercises {
 //        return result;
 //    }
 
-    public static String addBinary(String input0, String input1){
-
-        int numberOne = Integer.parseInt(input0, 2);
-        int numberTwo = Integer.parseInt(input1, 2);
-
-        int sum = numberOne + numberTwo;
-
-        return Integer.toBinaryString(sum);
-    }
-
+//    public static String addBinary(String input0, String input1){
+//
+//        int numberOne = Integer.parseInt(input0, 2);
+//        int numberTwo = Integer.parseInt(input1, 2);
+//
+//        int sum = numberOne + numberTwo;
+//
+//        return Integer.toBinaryString(sum);
+//    }
 }
